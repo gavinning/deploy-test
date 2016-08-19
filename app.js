86c5b1e390c910489c64227e5309d7bce6894caf
gavinning <ningyubo@gmail.com>
2016-08-19 13:06:07 +0800

    v1.1.0 新增aimee-is, aimee-extend, vpm-config依赖，用于测试依赖安装

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+var is = require('aimee-is')
+var extend = require('aimee-extend')
+var Config = require('vpm-config')
+
 var i = 0;
 
 setInterval(function(){
-	console.log(++i, 'deploy is work!')
+    console.log(++i + ' deploy is work!')
 }, 1000)
